9e378606c36501b9c308951d580c7e65d023a252
ankitapunekar24 <84794247+ankitapunekar24@users.noreply.github.com>
2025-07-07 10:05:31 +0100

    select_queries

---
+++ courses_select_update_7July.sql
@@ -0,0 +1,76 @@
+select * from courses
+
+--1st query
+
+select * from courses where fees >90000
+
+update courses set instructor = 'Ronak sir' where fees >90000
+
+select * from courses
+
+select coursename from courses where coursename = 'System Design'
+
+Delete from courses where coursename = 'System Design'
+
+select * from courses
+
+--2nd query
+
+select * from courses where studentname like 'M%' AND INSTRUCTOR= 'Anita Maam'
+
+UPDATE COURSES SET FEES = 90090 where studentname like 'M%' AND INSTRUCTOR= 'Anita Maam'
+
+select * from courses
+
+select * from courses where studentname = 'Ronak'
+
+delete from courses where studentname = 'Ronak'
+
+select * from courses
+
+--3rd query
+
+select * from courses where coursename = 'AIML'
+
+UPDATE courses set fees = 82000 where coursename = 'AIML'
+
+SELECT * FROM COURSES
+
+SELECT * FROM COURSES WHERE STUDENTNAME = 'Ankita' or studentname = 'Meghna'
+
+delete from courses WHERE STUDENTNAME = 'Ankita' or studentname = 'Meghna'
+
+select * from courses
+
+--4th query
+
+select * from courses where instructor = 'Sagar sir' or instructor = 'Anita Maam' and coursename = 'Hadoop'
+
+update courses set studentname = 'Sharmishtha' where instructor = 'Sagar sir' or instructor = 'Anita Maam' and coursename = 'Hadoop'
+
+SELECT * FROM COURSES
+
+SELECT * FROM courses WHERE coursename LIKE '%Data%' AND INSTRUCTOR = 'Rajesh Sir'
+
+delete from courses WHERE coursename LIKE '%Data%' AND INSTRUCTOR = 'Rajesh Sir'
+
+select * from courses
+
+--5th query
+
+SELECT * FROM courses WHERE fees > 80000;
+
+UPDATE courses SET fees = fees * 1.10 WHERE fees > 80000;
+
+select * from courses
+
+SELECT * FROM courses WHERE studentname LIKE 'A%'AND studentname like '%a'
+
+delete from courses WHERE studentname LIKE 'A%'AND studentname like '%a'
+
+select * from courses
+
+
+
+
+
